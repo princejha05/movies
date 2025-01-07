344982703738cfefaab6432c6cec071c91ac2b0c
princejha05 <163818937+princejha05@users.noreply.github.com>
2025-01-07 16:37:59 +0530

    Update cmds.py

--- plugins/Extra/cmds.py
+++ plugins/Extra/cmds.py
@@ -45,7 +45,7 @@ async def admin_cmds(client, message):
         [KeyboardButton("/del_file"), KeyboardButton("/delete")],
         [KeyboardButton("/deletefiles"), KeyboardButton("/deleteall")],
         [KeyboardButton("All These Commands Can Be Used Only By Admins.")],
-        [KeyboardButton("⚡")]
+        [KeyboardButton("⚡ @Prince_Jha_04")]
     ]
     reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True, one_time_keyboard=True)
    
